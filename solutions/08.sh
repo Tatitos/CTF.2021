c8d588fb7f7056d7b9daaf14d4bb5e38fcd0da7e
Sergio Lopez Jimenez <sergiolopezjimenez@MacBook-Pro-de-Sergio.local>
2021-09-16 10:18:02 +0200

    first solution for question 8

--- solutions/08.sh
+++ solutions/08.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# What is the path of the grep executable?
+# How many lines are in the file american-english-large?
 # Reponse: 51142 american-english-small
 
 wc -l american-english-small
